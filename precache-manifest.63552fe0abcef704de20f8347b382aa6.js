ebb1e01e5749bd746da80ae633e01312c8efec5c
pandatory <59501859+pandatory@users.noreply.github.com>
2020-02-17 17:49:20 +0900

    Updates

---
+++ precache-manifest.63552fe0abcef704de20f8347b382aa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06abae5ca07e686c58a678fdfc5c7847",
+    "url": "/luz_app_2020/index.html"
+  },
+  {
+    "revision": "e9fe477110cbf3e8bf6c",
+    "url": "/luz_app_2020/static/css/main.716dc537.chunk.css"
+  },
+  {
+    "revision": "44b73a87a21ac40b7884",
+    "url": "/luz_app_2020/static/js/2.d595e611.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/luz_app_2020/static/js/2.d595e611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9fe477110cbf3e8bf6c",
+    "url": "/luz_app_2020/static/js/main.27a11591.chunk.js"
+  },
+  {
+    "revision": "7b04d7a703c0848e4d26",
+    "url": "/luz_app_2020/static/js/runtime-main.1552e9a1.js"
+  }
+]);
